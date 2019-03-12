66004cb699b1aae114f7232f6ce155f178bf8244
Alice (TeplinskayaNA) <n.teplinskaya@bk.ru>
2019-03-12 14:47:57 +0300

    odin

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
 using namespace std;
-
-int main()
+int main ()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+
+ cout << "Enter A and B: ";
+ int a, b;
+cin >> a>> b;
+cout << "A+ B= " << a + b << '\n' << "A-B=" << a-b <<'\n';
+
+return 0;
 }
+
